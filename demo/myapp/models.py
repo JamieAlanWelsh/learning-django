b8520ab197ba6429811e11357f0d816c785cf610
Jamie Welsh <jw.jamiewelsh@gmail.com>
2024-11-27 08:59:34 +0000

    playing around with embedding a game

--- demo/myapp/models.py
+++ demo/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+# models are blueprint templates for DBs
+
 # Create your models here.
+class Tour(models.Model):
+    # origin country, destination, num nights, price
+    origin_country = models.CharField(max_length=64)
+    destination_country = models.CharField(max_length=64)
+    price = models.IntegerField()
